174deb7bd00c5e521e975c53a9c8bf5bf25da6bc
Nannerscube <nannersfive7@gmail.com>
2025-10-19 17:10:19 +0200

    Corrected seasonality math equation for months.

--- CyclicalEncodings.py
+++ CyclicalEncodings.py
@@ -1,5 +1,5 @@
 df["day_of_week_sin"] =np.sin(2 * np.pi * df["day_of_week"] / 7)
 df["day_of_week_cos"] =np.cos(2 * np.pi * df["day_of_week"] / 7)
 
-df["month_sin"] =np.sin(2 * np.pi * df["month"] / 7)
-df["month_cos"] =np.cos(2 * np.pi * df["month"] / 7)
+df["month_sin"] =np.sin(2 * np.pi * df["month"] / 12)
+df["month_cos"] =np.cos(2 * np.pi * df["month"] / 12)
